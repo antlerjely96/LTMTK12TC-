d59908de08c122d8105de3df1e8e4fb1e9760a88
antlerjely96 <antlerjely96@gmail.com>
2022-11-24 18:51:41 +0700

    add loop

---
+++ Loop/Loop/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Nhap so nguyen n. Tinh tong cac so tu 1 den n
+
+//Nhap n
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine());
+//Tinh tong
+int tong_for = 0;
+Console.WriteLine("------- for ---------");
+for (int i = 0; i < n; i++)
+{
+    tong_for += i;
+}
+Console.WriteLine("tong = " + tong_for);
+Console.WriteLine("------- while --------");
+int j = 0;
+int tong_while = 0;
+while (j < n)
+{
+    tong_while += j;
+    j++;
+}
+Console.WriteLine("tong = " + tong_while);
+Console.WriteLine("------- do - while --------");
+int k = 0;
+int tong_do_while = 0;
+do
+{
+    tong_do_while += k;
+    k++;
+} while(k < n);
+Console.WriteLine("tong = " + tong_do_while);
