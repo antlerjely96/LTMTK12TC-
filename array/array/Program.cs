88ad04854d2b28b70296c4c89a5f3aa694741cf4
antlerjely96 <antlerjely96@gmail.com>
2022-11-29 19:16:55 +0700

    add array vs method

---
+++ array/array/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* Nhap mot mang n phan tu. Tinh tong cac phan tu*/
+//Nhap n
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine());
+int tong = 0;
+//khai bao mang co n phan tu
+int[] arr = new int[n];
+//Nhap mang
+for (int i = 0; i < n; i++)
+{
+    Console.Write("arr[" + i + "] = ");
+    arr[i] = int.Parse(Console.ReadLine());
+}
+//Tinh tong cac phan tu
+for (int j = 0; j < n; j++)
+{
+    tong += arr[j];
+}
+//Hien thi tong
+Console.WriteLine("Tong cac phan tu cua mang: " + tong);
---
+++ method/method/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    //3 method
+    //Method nhap
+    public static int nhap()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+        return n;
+    }
+    //Method tinh n!
+    public static void tinhGiaiThua(int n)
+    {
+        int giaiThua = 1;
+        if (n < 0)
+        {
+            Console.WriteLine("n! khong ton tai");
+        }
+        else if(n == 0)
+        {
+            giaiThua = 1;
+        }
+        else
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                giaiThua *= i;
+            }
+        }
+        hienThi(giaiThua);
+    }
+    //Method hien thi
+    public static void hienThi(int giaiThua)
+    {
+        Console.WriteLine("n! = " + giaiThua);
+    }
+    public static void Main()
+    {
+        //Nhap mot so nguyen n. Tinh n!
+        int n = nhap();
+        tinhGiaiThua(n);
+    }
+}
