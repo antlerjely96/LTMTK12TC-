acd7cf847337ff1f40e901427d913ac80e3d21e3
antlerjely96 <antlerjely96@gmail.com>
2022-12-01 20:45:04 +0700

    add new class

---
+++ classes/classes/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using classes;
+
+public class Program
+{
+    public static void Main()
+    {
+        Students student1 = new Students();
+        student1.input();
+        student1.output();
+        Students[] studentsArray = new Students[20];
+    }
+}
---
+++ classes/classes/Students.cs
@@ -0,0 +1,34 @@
+﻿namespace classes;
+
+public class Students
+{
+    public string id;
+    public string name;
+    public string phone;
+    public int age;
+    public string address;
+
+    public void input()
+    {
+        Console.Write("ID: ");
+        this.id = Console.ReadLine();
+        Console.Write("Name: ");
+        this.name = Console.ReadLine();
+        Console.Write("Phone: ");
+        this.phone = Console.ReadLine();
+        Console.Write("Age: ");
+        this.age = int.Parse(Console.ReadLine());
+        Console.Write("Address: ");
+        this.address = Console.ReadLine();
+    }
+
+    public void output()
+    {
+        Console.WriteLine("Student Information:");
+        Console.WriteLine("ID: " + this.id);
+        Console.WriteLine("name: " + this.name);
+        Console.WriteLine("phone: " + this.phone);
+        Console.WriteLine("Age: " + this.age);
+        Console.WriteLine("address: " + this.address);
+    }
+}
